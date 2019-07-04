9897e219df680401680b5a12aa33264f8b1e6098
Diego Chaves Castro <diego1409@users.noreply.github.com>
2019-07-03 23:36:33 -0600

    Agregar clase para procedimientos almacenados

---
+++ app/nutritionApp/Clases/clsProcedimientosAlmacenados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Referencias a las librerías necesarias
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+
+namespace nutritionApp.Clases
+{
+    public class clsProcedimientosAlmacenados
+    {
+    }
+}
