085bfee46ee39c9c64fdc7bc3c3357b9283268c4
Diego Chaves Castro <diego1409@users.noreply.github.com>
2019-07-07 02:33:22 -0600

    Creacion de master page e Index + Estilos

---
+++ app/nutritionApp/src/aspx/Index.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="Inicio" Language="C#" MasterPageFile="~/src/aspx/masterPage.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="nutritionApp.src.aspx.frmIndex" %>
+<asp:Content ID="body" ContentPlaceHolderID="body" runat="server">
+</asp:Content>
---
+++ app/nutritionApp/src/aspx/masterPage.Master.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace nutritionApp.src.aspx {
+    
+    
+    public partial class masterPage {
+        
+        /// <summary>
+        /// Control body.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder body;
+        
+        /// <summary>
+        /// Control frmScripts.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm frmScripts;
+        
+        /// <summary>
+        /// Control scmScripts.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager scmScripts;
+    }
+}
