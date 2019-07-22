d86b8df807ba533d71c8e15af8fe6d0a78c9a097
Diego Chaves Castro <diego1409@users.noreply.github.com>
2019-07-21 22:50:13 -0600

    Cambios en archivos .sql
    Actualizar scripts y separar sp en cada archivo

--- bd/sp/InsertaUsuario.sql
+++ bd/sp/InsertaUsuario.sql
@@ -35,31 +35,3 @@ BEGIN
 	Values (NULL,@cedula,@genero,@fechaNac,@nombre,@apellido1,@apellido2,@direccion,@telefono1,@estatura,@peso,@proposito,@correo,@usuario,@contrasena,@tipoUsuario)
 END
 GO
-
-CREATE OR ALTER PROCEDURE [dbo].[ModificaUsuario]
-	@id_usuario int,
-	@foto image NULL,
-	@cedula varchar(20),
-	@genero char(1),
-	@fechaNac date,
-	@nombre varchar(50),
-	@apellido1 varchar(50),
-	@apellido2 varchar(50) NULL,
-	@direccion varchar(100),
-	@telefono1 varchar (10),
-	@estatura tinyint,
-	@peso decimal(5,2),
-	@proposito varchar(50),
-	@correo varchar(50),
-	@usuario varchar(20),
-	@contrasena varchar(50),
-	@tipoUsuario char(1)
-AS
-BEGIN
-	Update usuario
-	set foto=@foto, cedula=@cedula, genero=@genero, fechaNac=@fechaNac, nombre=@nombre, apellido1=@apellido1, 
-	apellido2=@apellido2, direccion=@direccion, telefono1=@telefono1, estatura=@estatura,
-	peso=@peso,proposito=@proposito, correo=@correo, usuario=@usuario, contrasena=@contrasena, tipoUsuario=@tipoUsuario
-	WHERE idUsuario=@id_usuario
-END
-GO
---
+++ bd/sp/ModificaUsuario.sql
@@ -0,0 +1,35 @@
+
+CREATE   PROCEDURE [dbo].[ModificaUsuario]
+	--@foto image NULL,
+	@cedula varchar(20),
+	@genero char(1),
+	@fechaNac date,
+	@nombre varchar(50),
+	@apellido1 varchar(50),
+	@apellido2 varchar(50) NULL,
+	@direccion varchar(100),
+	@telefono1 varchar (10),
+	@estatura tinyint,
+	@peso real,
+	@proposito varchar(50),
+	@correo varchar(50),
+	@usuario varchar(20),
+	@contrasena varchar(50) NULL,
+	@tipoUsuario char(1)
+AS
+BEGIN
+	
+	Update usuario
+		set genero=@genero, fechaNac=@fechaNac, nombre=@nombre, apellido1=@apellido1, 
+		apellido2=@apellido2, direccion=@direccion, telefono1=@telefono1, estatura=@estatura,
+		peso=@peso,proposito=@proposito, correo=@correo, usuario=@usuario, contrasena=@contrasena, tipoUsuario=@tipoUsuario
+		WHERE cedula=@cedula
+
+	if @contrasena IS NOT NULL
+	BEGIN
+		Update usuario
+		set contrasena=@contrasena
+		WHERE cedula=@cedula
+	END
+END
+GO
