5776218a94ddffa01d81ea9f16d5e865c69404b7
leoaguilarm <leonardoaguil@hotmail.com>
2019-07-22 18:41:09 -0600

    Sincronizacion

---
+++ app/nutritionApp/src/aspx/frmIMC.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//Clases de conexion
+using nutritionApp.Clases;
+
+namespace nutritionApp.src.aspx
+{
+    public partial class frmIMC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //se pregunta si no es una recarga de pagina...
+            //validar si es la primera vez que carga la pagina
+            //para que no cargue las listas cuando se da click
+            //en el boton de guardar
+            if (!this.IsPostBack)
+            {
+                
+            }
+        }
+    }
+}
